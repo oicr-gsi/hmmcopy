6e3dcad28cac7f2c815740a22446fec879ca19e6
pruzanov <pruzanov@oicr.on.ca>
2021-11-09 09:47:58 -0500

    Making calculate RT script more robust

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -4,17 +4,17 @@ cd $1
 find .  -name "*\.zip" -exec unzip -q {} \; >/dev/null # unzip the results files
 
 # - .tsv files have no stochastic content, may be md5sum-checked
-# - .seg files are generically named, no stochastic content, may be md5sum-checked
-# - *.png files should be generating the same md5sum, apparantly no stochastic content
+# - .seg files are generically named, there may be differences at pos 10 after the decimal point, so we round to 3 digits
+# - *.png files should be generating the same md5sum, apparantly no stochastic content except for bias plot which we are not checkong
 
 # Therefore:
-# - Check md5sums for all types of files, sort
+# - Check md5sums for all types of files (where possible), sort
+
+echo ".seg files:"
+for f in *.seg;do awk '{printf "%s %s %i %i %.3f\n", $1, $2, $3, $4, $5}' $f | md5sum;done | sort -V
 
 echo ".tsv files:"
 find . -name "*.tsv" | xargs md5sum | sort -V
 
-echo ".seg files:"
-find . -name "*.seg" | xargs md5sum | sort -V
-
 echo ".png files:"
-find . -name "*.png" | xargs md5sum | sort -V
+find . -name "*.png" | grep -v bias | xargs md5sum | sort -V
